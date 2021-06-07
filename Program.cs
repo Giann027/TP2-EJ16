c98148ee198dab4afab2bce598fd59247f5af73a
ricot <ricot@DESKTOP-FTHKA14>
2021-06-07 20:07:58 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TP2_EJ16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BUENASS!");
+            Console.Title = "Designar Número par o impar ";
+            Console.Write("Ingrese el Número Entero a verificar ");
+            var Número = Convert.ToSingle(Console.ReadLine());
+
+            if (Número % 2 == 0)
+            
+                Console.Write("El Número ingresado es Par ");
+            
+            else
+                Console.Write("El Número ingresado es Impar ");
+        }
+    }
+}
